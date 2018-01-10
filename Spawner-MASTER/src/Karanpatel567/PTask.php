85f46c9a84041d117ff3dcda7ae0df443d87b73c
ZPlayzMCPE <jameshayward3800@gmail.com>
2018-01-10 10:33:07 +0000

    Adding more code to PTask.

--- Spawner-MASTER/src/Karanpatel567/PTask.php
+++ Spawner-MASTER/src/Karanpatel567/PTask.php
@@ -27,5 +27,38 @@ class PTask extends PluginTask {
   */
  public function onRun(int $tick): void{
   $tile = $this->block->getLevel()->getTile(new Vector3($this->block->getX(), $this->block->getY(), $this->block->getZ()));
-  
-  
+  if($tile instanceof MobSpawner) {
+   echo "Translating to {$this->type}...";
+   switch($this->type){
+    case "zombie":
+     $tile->setEntityId(32);
+     break;
+    case "pigman":
+     $tile->setEntityId(36);
+     break;
+    case "spider":
+     $tile->setEntityId(35);
+     break;
+    case "iron_golem":
+     $tile->setEntityId(20);
+     break;
+    case "blaze":
+     $tile->setEntityId(43);
+     break;
+    case "pig":
+     $tile->setEntityId(12);
+     break;
+    case "cow":
+     $tile->setEntityId(11);
+     break;
+    case "chicken":
+     $tile->setEntityId(10);
+     break;
+    case "squid":
+     $tile->setEntityId(17);
+     break;
+   }
+  }
+ }
+
+}
