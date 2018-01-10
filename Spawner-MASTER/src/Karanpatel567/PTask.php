22d8656b198b56696cad24f929a38593ef0e2cb1
ZPlayzMCPE <jameshayward3800@gmail.com>
2018-01-10 10:28:33 +0000

    Re-formatting file.

---
+++ Spawner-MASTER/src/Karanpatel567/PTask.php
@@ -0,0 +1,25 @@
+<?php
+
+class PTask extends PluginTask{
+ 
+ /** @var Main */
+ public $plugin;
+ /** @var Block */
+ public $block;
+ /** @var string */
+ public $type;
+ 
+ public function __construct(Main $plugin, Block $block, string $type){
+  parent::__construct($plugin);
+  $this->plugin = $plugin;
+  $this->block = $block;
+  $this->type = $type;
+ }
+ 
+ /**
+  * @param int $tick
+  *
+  * @return void
+  */
+ public function onRun(int $tick): void{
+  $tile = $this->block->getLevel()->getTile(new Vector3($this->block->getX(), $this->block->getY(), $this->block->getZ()));
